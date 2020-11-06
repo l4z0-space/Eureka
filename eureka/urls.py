34107e4b196fe280071e8a7b535cb6c382d435d4
Dastan <dastanbeksamatov@gmail.com>
2020-11-06 02:38:26 +0200

    Fix urls

--- eureka/urls.py
+++ eureka/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('api/accounts/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
